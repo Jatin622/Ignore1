b2e3ac80eda7e8ee1c58487836d79d6e7e4452ff
Jatin622 <mangla.jatin2015@gmail.com>
2022-11-02 09:53:54 +0530

    added new files to repository

---
+++ src/main/java/com/ignoreCase/IgnoreCaseApplication.java
@@ -0,0 +1,13 @@
+package com.ignoreCase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IgnoreCaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IgnoreCaseApplication.class, args);
+	}
+
+}
